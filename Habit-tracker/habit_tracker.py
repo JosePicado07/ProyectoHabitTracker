23f6ae7b2d77deaaf71bc5058672284228d8e2e1
JosePicado07 <142560023+JosePicado07@users.noreply.github.com>
2024-09-17 20:30:54 -0600

    Cambios en el habit tracker, comenzamos a trabajar con AWS Lambda, API y DynamoDB

--- Habit-tracker/habit_tracker.py
+++ Habit-tracker/habit_tracker.py
@@ -1,18 +1,30 @@
+import uuid
+from datetime import datetime
+
+
 class HabitTracker:
     def __init__(self):
         self.habits = {}
 
-    def add_habit(self,habit_name):
+    def add_habit(self, habit_name, frequency="diaria", start_date=None):
         if habit_name not in self.habits:
-            self.habits[habit_name] = False
-            return f"Habito '{habit_name}' anadido"
+            habit_id = str(uuid.uuid4())  # Se genera un identifier único
+            start_date = start_date or datetime.now().strftime('%Y-%m-%d')
+            self.habits[habit_name] = {
+                "id": habit_id,
+                "frequency": frequency,
+                "start_date": start_date,
+                "completed": False
+            }
+            return f"Hábito '{habit_name}' añadido con ID {habit_id}"
         else:
-            return f"El habito '{habit_name}' ya existe"
+            return f"El hábito '{habit_name}' ya existe"
 
     def complete_habit(self,habit_name):
         if habit_name in self.habits:
             self.habits[habit_name] = True
-            return f"Habito '{habit_name}' marcado como completado"
+            return (f"Habito "
+                    f"'{habit_name}' marcado como completado")
         else:
             return f"El habito '{habit_name}' no existe"
 
@@ -25,5 +37,16 @@ class HabitTracker:
 
     def list_habits(self):
         if not self.habits:
-            return "No hay habitos registrados"
-        return "\n".join(f"{habit}: {'Completado' if status else 'No completado'}" for habit, status in self.habits.items())
+            return "No hay hábitos registrados"
+
+        habit_list = []
+        for habit_name, details in self.habits.items():
+            status = "Completado" if details['completed'] else "No completado"
+            habit_info = (f"Hábito: {habit_name}\n"
+                          f"ID: {details['id']}\n"
+                          f"Frecuencia: {details['frequency']}\n"
+                          f"Fecha de inicio: {details['start_date']}\n"
+                          f"Estado: {status}\n")
+            habit_list.append(habit_info)
+
+        return "\n".join(habit_list)
