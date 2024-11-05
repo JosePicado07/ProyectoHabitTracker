e4f434d7e104e9fd27367f00df4c7c761608398c
JosePicado07 <142560023+JosePicado07@users.noreply.github.com>
2024-11-05 14:25:49 -0600

    Delete Habit-tracker/src/config_py.py

--- Habit-tracker/src/config_py.py
+++
@@ -1,6 +0,0 @@
-# config.py
-
-USER_POOL_ID = 'us-east-2_U0WJWAGiX'  # ID del grupo de usuarios de Cognito
-CLIENT_ID = '6rsahhiqvv151hacg77qvutajl'  # ID del cliente.
-REGION = 'us-east-2'  # Región de AWS
-API_URL = "https://mprtojru2b.execute-api.us-east-2.amazonaws.com"
